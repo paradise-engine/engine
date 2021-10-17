e7c26b534c6cc7b7149f311e9dd49b809ec1c1c4
Taylan Kenanoglu <taylan.kenanoglu@outlook.de>
2021-10-17 18:42:25 +0200

    moved tooling to webpack

---
+++ webpack.config.ts
@@ -0,0 +1,49 @@
+import path from 'path';
+import { Configuration } from 'webpack';
+
+const config: Configuration = {
+    entry: './src/index.ts',
+    devtool: 'source-map',
+    optimization: {
+        minimize: false,
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                enforce: 'pre',
+                use: ['source-map-loader']
+            },
+            {
+                test: /\.tsx?$/,
+                enforce: 'pre',
+                use: ['source-map-loader']
+            },
+            {
+                test: /\.tsx?$/,
+                use: [{
+                    loader: 'ts-loader',
+                    options: {
+                        configFile: "tsconfig.json"
+                    }
+                }],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(vert|frag)/,
+                type: 'asset/source',
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'engine.js',
+        path: path.resolve(__dirname, 'dist'),
+        libraryTarget: 'umd'
+    }
+}
+
+export default config;
