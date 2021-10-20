1f1b24ec9f3853308fa198495ba2af0dcc316609
Taylan Kenanoglu <taylan.kenanoglu@outlook.de>
2021-10-20 04:51:56 +0200

    renamed Rectangle to Rect in tests, too

--- test/core/rect.spec.ts
+++ test/core/rect.spec.ts
@@ -1,9 +1,9 @@
-import { Rectangle } from '../../src';
+import { Rect } from '../../src';
 
 describe('Rectangle instance', () => {
 
     it('takes all zeroes as default', () => {
-        const rec = new Rectangle();
+        const rec = new Rect();
         expect(rec.x).toBe(0);
         expect(rec.y).toBe(0);
         expect(rec.width).toBe(0);
@@ -11,7 +11,7 @@ describe('Rectangle instance', () => {
     });
 
     it('correctly stores initial values', () => {
-        const rec = new Rectangle(10, 20, 500, 100);
+        const rec = new Rect(10, 20, 500, 100);
         expect(rec.x).toBe(10);
         expect(rec.y).toBe(20);
         expect(rec.width).toBe(500);
@@ -19,19 +19,19 @@ describe('Rectangle instance', () => {
     });
 
     it('evaluates equality correctly', () => {
-        let r1 = new Rectangle();
-        let r2 = new Rectangle(0, 0, 0, 0);
+        let r1 = new Rect();
+        let r2 = new Rect(0, 0, 0, 0);
 
         expect(r1.equals(r2)).toBe(true);
         expect(r2.equals(r1)).toBe(true);
 
-        r1 = new Rectangle(10, 20, 30, 40);
-        r2 = new Rectangle(10, 20, 30, 40);
+        r1 = new Rect(10, 20, 30, 40);
+        r2 = new Rect(10, 20, 30, 40);
 
         expect(r1.equals(r2)).toBe(true);
         expect(r2.equals(r1)).toBe(true);
 
-        r2 = new Rectangle(11, 20, 30, 40);
+        r2 = new Rect(11, 20, 30, 40);
 
         expect(r1.equals(r2)).toBe(false);
         expect(r2.equals(r1)).toBe(false);
