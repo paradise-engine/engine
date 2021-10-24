da28243ad8fdb61a256cb6025264964c7bdab27c
Taylan Kenanoglu <taylan.kenanoglu@outlook.de>
2021-10-24 02:32:47 +0200

    added some members to renderer

--- src/renderer/renderer.ts
+++ src/renderer/renderer.ts
@@ -1,6 +1,8 @@
 import { Shader } from "../webgl";
 import { RenderingContextError } from "../errors";
-import { BaseShader } from '../shader';
+import { DefaultShader } from '../shader';
+import { ShaderPipeline } from "./shader-pipeline";
+import { GlobalShaderData } from "./global-shader-data";
 
 export interface RendererOptions {
     view?: HTMLCanvasElement;
@@ -18,7 +20,9 @@ export class Renderer {
     public readonly width: number;
     public readonly height: number;
 
+    public readonly globalShaderData: GlobalShaderData;
     public readonly baseShader: Shader;
+    public readonly shaderPipeline: ShaderPipeline;
 
     constructor(options?: RendererOptions) {
         options = options || {};
@@ -40,6 +44,8 @@ export class Renderer {
         this.view.width = this.width;
         this.view.height = this.height;
 
-        this.baseShader = options.baseShader || new BaseShader(this);
+        this.globalShaderData = new GlobalShaderData(this);
+        this.baseShader = options.baseShader || new DefaultShader(this);
+        this.shaderPipeline = new ShaderPipeline(this);
     }
 }
