78e3f99cc4875b01995568e1a49368fd84afde8f
Taylan Kenanoglu <taylan.kenanoglu@outlook.de>
2021-10-27 23:42:59 +0200

    implemented renderable sprite

--- src/renderer/renderable-primitive.ts
+++ src/renderer/renderable-primitive.ts
@@ -1,25 +1,59 @@
-import { Rotation, Vector } from "../core";
 import { Shader } from "../webgl";
 import { IRenderable } from "./i-renderable";
 import { Renderer } from "./renderer";
+import { mat4, quat, vec3 } from "gl-matrix";
 
 export abstract class RenderablePrimitive implements IRenderable {
     protected _shaders: Shader[] = [];
 
-    public position: Vector;
-    public rotation: Rotation;
-    public scale: Vector;
+    private _globalMatrix: mat4;
+    private _translation: vec3 = vec3.create();
+    private _rotation: quat = quat.create();
+    private _scaling: vec3 = vec3.create();
 
-    constructor(position: Vector, rotation: Rotation, scale: Vector) {
-        this.position = position;
-        this.rotation = rotation;
-        this.scale = scale;
+    public get globalMatrix() {
+        return this._globalMatrix;
+    }
+
+    public set globalMatrix(value: mat4) {
+        this._globalMatrix = value;
+        this._updateTransforms();
+    }
+
+    public get x() {
+        return this._translation[0];
+    }
+
+    public get y() {
+        return this._translation[1];
+    }
+
+    public get scaleX() {
+        return this._scaling[0];
+    }
+
+    public get scaleY() {
+        return this._scaling[1];
+    }
+
+    public get rotationZ() {
+        return this._rotation[2];
+    }
+
+    constructor(globalMatrix: mat4) {
+        this._globalMatrix = globalMatrix;
+    }
+
+    private _updateTransforms() {
+        mat4.getTranslation(this._translation, this._globalMatrix);
+        mat4.getRotation(this._rotation, this._globalMatrix);
+        mat4.getScaling(this._scaling, this._globalMatrix);
     }
 
     public abstract render(renderer: Renderer): void;
 
     public getShaders() {
-        return this._shaders.concat();
+        return this._shaders.slice();
     }
 
     public addShader(shader: Shader) {
--- src/renderer/sprite.ts
+++ src/renderer/sprite.ts
@@ -1,4 +1,5 @@
-import { Rotation, Vector } from "../core";
+import { mat4 } from "gl-matrix";
+import { drawImage, DrawImageOptions } from "../webgl";
 import { RenderablePrimitive } from "./renderable-primitive";
 import { Renderer } from "./renderer";
 import { Texture } from "./texture";
@@ -6,17 +7,33 @@ import { Texture } from "./texture";
 export class Sprite extends RenderablePrimitive {
     public texture: Texture;
 
-    constructor(texture: Texture) {
-        super(
-            new Vector(0, 0),
-            Rotation.fromDegrees(0),
-            new Vector(1, 1)
-        );
+    constructor(texture: Texture, globalMatrix?: mat4) {
+        super(globalMatrix || mat4.create());
         this.texture = texture;
     }
 
     render(renderer: Renderer) {
         const textureToRender = renderer.shaderPipeline.applyShaders(this.texture.baseTexture, this.getShaders());
 
+        const drawOptions: DrawImageOptions = {
+            gl: renderer.context,
+            shader: renderer.baseShader,
+            globalUniforms: renderer.globalShaderData.uniforms,
+            texture: textureToRender,
+            textureWidth: this.texture.baseTexture.width,
+            textureHeight: this.texture.baseTexture.height,
+            sourceX: this.texture.frame.x,
+            sourceY: this.texture.frame.y,
+            sourceWidth: this.texture.frame.width,
+            sourceHeight: this.texture.frame.height,
+
+            destinationX: this.x,
+            destinationY: this.y,
+            destinationWidth: this.scaleX * this.texture.frame.width,
+            destinationHeight: this.scaleY * this.texture.frame.height,
+            rotationRadian: this.rotationZ
+        };
+
+        drawImage(drawOptions);
     }
 }
